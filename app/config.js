5684283037153dbae6012896501f9d548a0adf14
Nick Knudsen <nickknudsen3@gmail.com>
2019-04-27 12:05:00 -0500

    Added middleware and config files. Note config file will not be in the production environment, it is only for testing

---
+++ app/config.js
@@ -0,0 +1,8 @@
+// This file stores the secret for the JWT hashing
+// this should be stored in an environment variable
+// on the production server.
+
+module.exports = {
+	secret: 'teslaworksisamazing'
+};
+
