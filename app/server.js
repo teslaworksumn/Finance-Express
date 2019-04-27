e2056f057a15cc69958debf2fd0630404d8bd55b
Nick Knudsen <nickknudsen3@gmail.com>
2019-04-27 12:04:36 -0500

    Updated spacing

--- app/server.js
+++ app/server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello world!')
+  res.send('Hello world!')
 });
 
 app.post('/', (req, res) => {
-	res.send('This is a POST request!')
+  res.send('This is a POST request!')
 });
 
 app.listen(8000, () => {
-	console.log('Server listening on port 8000')
+  console.log('Server listening on port 8000')
 });
